36a65d6e559e0269ad431ebcb43e7ec49d90a33b
messense <messense@icloud.com>
2018-04-17 23:37:39 +0800

    Update tests

--- tests/test_mmseg.rs
+++ tests/test_mmseg.rs
@@ -3,8 +3,10 @@ extern crate mmseg;
 use mmseg::MMSeg;
 
 #[test]
-fn test_mmseg_load_dict() {
+fn test_mmseg() {
     let mmseg = MMSeg::new();
-    let ret = mmseg.cut_simple("研究生命来源, this is a test 1988/02/29");
-    println!("{:#?}", ret);
+    let simple = mmseg.cut_simple("研究生命来源, this is a test 1988/02/29");
+    println!("simple: {:#?}", simple);
+    let complex = mmseg.cut("我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。");
+    println!("complex: {:#?}", complex);
 }
